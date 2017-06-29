b43c5517a0e22d4dd125bada7a9082510ef23269
jonathadev <jonatha.mendonca@hotmail.com>
2017-06-28 21:45:34 -0300

    first commit sistema crud c sharp devmedia

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FrmCadastroCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCadastroCliente));
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.mskCEP = new System.Windows.Forms.MaskedTextBox();
+            this.mskTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbNovo = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalvar = new System.Windows.Forms.ToolStripButton();
+            this.tsbCancelar = new System.Windows.Forms.ToolStripButton();
+            this.tsbExcluir = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbBuscaPorId = new System.Windows.Forms.ToolStripLabel();
+            this.tstId = new System.Windows.Forms.ToolStripTextBox();
+            this.tsbBuscar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(22, 70);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(172, 70);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Location = new System.Drawing.Point(22, 119);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(53, 13);
+            this.lblEndereco.TabIndex = 2;
+            this.lblEndereco.Text = "Endereco";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(25, 91);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // txtNome
+            // 
+            this.txtNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNome.Location = new System.Drawing.Point(175, 91);
+            this.txtNome.MaxLength = 50;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(283, 20);
+            this.txtNome.TabIndex = 8;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtEndereco.Location = new System.Drawing.Point(25, 135);
+            this.txtEndereco.MaxLength = 50;
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(433, 20);
+            this.txtEndereco.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "CEP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "UF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Bairro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(296, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Cidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Telefone";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBairro.Location = new System.Drawing.Point(162, 200);
+            this.txtBairro.MaxLength = 50;
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(100, 20);
+            this.txtBairro.TabIndex = 16;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtCidade.Location = new System.Drawing.Point(299, 200);
+            this.txtCidade.MaxLength = 50;
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(100, 20);
+            this.txtCidade.TabIndex = 17;
+            // 
+            // txtUF
+            // 
+            this.txtUF.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtUF.Location = new System.Drawing.Point(419, 200);
+            this.txtUF.MaxLength = 2;
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(39, 20);
+            this.txtUF.TabIndex = 18;
+            // 
+            // mskCEP
+            // 
+            this.mskCEP.Location = new System.Drawing.Point(25, 200);
+            this.mskCEP.Mask = "00000-999";
+            this.mskCEP.Name = "mskCEP";
+            this.mskCEP.Size = new System.Drawing.Size(100, 20);
+            this.mskCEP.TabIndex = 20;
+            // 
+            // mskTelefone
+            // 
+            this.mskTelefone.Location = new System.Drawing.Point(25, 262);
+            this.mskTelefone.Mask = "(99) 0 0000-0000";
+            this.mskTelefone.Name = "mskTelefone";
+            this.mskTelefone.Size = new System.Drawing.Size(100, 20);
+            this.mskTelefone.TabIndex = 21;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNovo,
+            this.tsbSalvar,
+            this.tsbCancelar,
+            this.tsbExcluir,
+            this.toolStripSeparator1,
+            this.tsbBuscaPorId,
+            this.tstId,
+            this.tsbBuscar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(485, 25);
+            this.toolStrip1.TabIndex = 22;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbNovo
+            // 
+            this.tsbNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNovo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNovo.Image")));
+            this.tsbNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNovo.Name = "tsbNovo";
+            this.tsbNovo.Size = new System.Drawing.Size(23, 22);
+            this.tsbNovo.Text = "Novo";
+            this.tsbNovo.Click += new System.EventHandler(this.tsbNovo_Click);
+            // 
+            // tsbSalvar
+            // 
+            this.tsbSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbSalvar.Image = ((System.Drawing.Image)(resources.GetObject("tsbSalvar.Image")));
+            this.tsbSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalvar.Name = "tsbSalvar";
+            this.tsbSalvar.Size = new System.Drawing.Size(23, 22);
+            this.tsbSalvar.Text = "Salvar";
+            this.tsbSalvar.Click += new System.EventHandler(this.tsbSalvar_Click);
+            // 
+            // tsbCancelar
+            // 
+            this.tsbCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbCancelar.Image = ((System.Drawing.Image)(resources.GetObject("tsbCancelar.Image")));
+            this.tsbCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbCancelar.Name = "tsbCancelar";
+            this.tsbCancelar.Size = new System.Drawing.Size(23, 22);
+            this.tsbCancelar.Text = "Cancelar";
+            this.tsbCancelar.Click += new System.EventHandler(this.tsbCancelar_Click);
+            // 
+            // tsbExcluir
+            // 
+            this.tsbExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbExcluir.Image = ((System.Drawing.Image)(resources.GetObject("tsbExcluir.Image")));
+            this.tsbExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExcluir.Name = "tsbExcluir";
+            this.tsbExcluir.Size = new System.Drawing.Size(23, 22);
+            this.tsbExcluir.Text = "Excluir";
+            this.tsbExcluir.Click += new System.EventHandler(this.tsbExcluir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsbBuscaPorId
+            // 
+            this.tsbBuscaPorId.Name = "tsbBuscaPorId";
+            this.tsbBuscaPorId.Size = new System.Drawing.Size(75, 22);
+            this.tsbBuscaPorId.Text = "Busca por Id:";
+            // 
+            // tstId
+            // 
+            this.tstId.Name = "tstId";
+            this.tstId.Size = new System.Drawing.Size(100, 25);
+            // 
+            // tsbBuscar
+            // 
+            this.tsbBuscar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbBuscar.Image = ((System.Drawing.Image)(resources.GetObject("tsbBuscar.Image")));
+            this.tsbBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBuscar.Name = "tsbBuscar";
+            this.tsbBuscar.Size = new System.Drawing.Size(23, 22);
+            this.tsbBuscar.Text = "Buscar";
+            this.tsbBuscar.Click += new System.EventHandler(this.tsbBuscar_Click);
+            // 
+            // FrmCadastroCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 293);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.mskTelefone);
+            this.Controls.Add(this.mskCEP);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblId);
+            this.Name = "FrmCadastroCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.FrmCliente_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.MaskedTextBox mskCEP;
+        private System.Windows.Forms.MaskedTextBox mskTelefone;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbNovo;
+        private System.Windows.Forms.ToolStripButton tsbSalvar;
+        private System.Windows.Forms.ToolStripButton tsbCancelar;
+        private System.Windows.Forms.ToolStripButton tsbExcluir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel tsbBuscaPorId;
+        private System.Windows.Forms.ToolStripTextBox tstId;
+        private System.Windows.Forms.ToolStripButton tsbBuscar;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+     
+    public partial class FrmCadastroCliente : Form
+    {
+
+        string connectionString = @"Server=.\sqlexpress;Database=BDCADASTRO;Trusted_Connection=True;";
+        bool novo;
+
+
+        public FrmCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCliente_Load(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtUF.Enabled = false;
+            mskTelefone.Enabled = false;
+        }
+
+        private void tsbNovo_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = false;
+            tsbSalvar.Enabled = true;
+            tsbCancelar.Enabled = true;
+            tsbExcluir.Enabled = false;
+            tstId.Enabled = false;
+            tsbBuscar.Enabled = false;
+            txtNome.Enabled = true;
+            txtEndereco.Enabled = true;
+            mskCEP.Enabled = true;
+            txtBairro.Enabled = true;
+            txtCidade.Enabled = true;
+            txtUF.Enabled = true;
+            mskTelefone.Enabled = true;
+            txtNome.Focus();
+            novo = true;
+        }
+
+        private void tsbSalvar_Click(object sender, EventArgs e)
+        {
+            if (novo)
+              {
+                string sql = "INSERT INTO CLIENTE (ID, NOME,ENDERECO,CEP,BAIRRO,CIDADE,UF,TELEFONE) "
+                + "VALUES ('" + txtId.Text + "', '" + txtNome.Text + "', '" + txtEndereco.Text + "', '" 
+                + mskCEP.Text + "', '" + txtBairro.Text
+                + "', '" + txtCidade.Text + "', '" + txtUF.Text + "', '" 
+                + mskTelefone.Text + "')";
+              
+                  SqlConnection con = new SqlConnection(connectionString);
+                  SqlCommand cmd = new SqlCommand(sql, con);
+                  cmd.CommandType = CommandType.Text;
+                  con.Open();
+                  try
+                  {
+                      int i = cmd.ExecuteNonQuery();
+                      if (i > 0)
+                          MessageBox.Show("Cadastro realizado com sucesso!");
+                  }
+                  catch (Exception ex)
+                  {
+                      MessageBox.Show("Erro: " + ex.ToString());
+                  }
+                  finally
+                  {
+                      con.Close();
+                  }
+              }
+              else
+              {
+                string sql = "UPDATE CLIENTE SET NOME='" + txtNome.Text + "', ENDERECO='" + txtEndereco.Text + "', " +
+                 "CEP='" + mskCEP.Text + "', BAIRRO='" + txtBairro.Text + "', CIDADE='" + txtCidade.Text + "', " +
+                 "UF='" + txtUF.Text + "', TELEFONE='" + mskTelefone.Text + "'";
+   
+                  SqlConnection con = new SqlConnection(connectionString);
+                  SqlCommand cmd = new SqlCommand(sql, con);
+                  cmd.CommandType = CommandType.Text;
+                  con.Open();
+                  try
+                  {
+                      int i = cmd.ExecuteNonQuery();
+                      if (i > 0)
+                          MessageBox.Show("Cadastro atualizado com sucesso!");
+                  }
+                  catch (Exception ex)
+                  {
+                      MessageBox.Show("Erro: " + ex.ToString());
+                  }
+                  finally
+                  {
+                      con.Close();
+                  }
+              }
+   
+              tsbNovo.Enabled = true;
+              tsbSalvar.Enabled = false;
+              tsbCancelar.Enabled = false;
+              tsbExcluir.Enabled = false;
+              tstId.Enabled = true;
+              tsbBuscar.Enabled = true;
+              txtNome.Enabled = false;
+              txtEndereco.Enabled = false;
+              mskCEP.Enabled = false;
+              txtBairro.Enabled = false;
+              txtCidade.Enabled = false;
+              txtUF.Enabled = false;
+              mskTelefone.Enabled = false;
+              txtId.Text = "";
+              txtNome.Text = "";
+              txtEndereco.Text = "";
+              mskCEP.Text = "";
+              txtBairro.Text = "";
+              txtCidade.Text = "";
+              txtUF.Text = "";
+              mskTelefone.Text = "";
+   
+        }
+
+        private void tsbCancelar_Click(object sender, EventArgs e)
+        {
+           
+              tsbNovo.Enabled = true;
+              tsbSalvar.Enabled = false;
+              tsbCancelar.Enabled = false;
+              tsbExcluir.Enabled = false;
+              tstId.Enabled = true;
+              tsbBuscar.Enabled = true;
+              txtNome.Enabled = false;
+              txtEndereco.Enabled = false;
+              mskCEP.Enabled = false;
+              txtBairro.Enabled = false;
+              txtCidade.Enabled = false;
+              txtUF.Enabled = false;
+              mskTelefone.Enabled = false;
+              txtId.Text = "";
+              txtNome.Text = "";
+              txtEndereco.Text = "";
+              mskCEP.Text = "";
+              txtBairro.Text = "";
+              txtCidade.Text = "";
+              txtUF.Text = "";
+              mskTelefone.Text = "";
+   
+          
+        }
+
+        private void tsbExcluir_Click(object sender, EventArgs e)
+        {
+            
+   
+              string sql = "DELETE FROM CLIENTE WHERE ID=" + txtId.Text;
+   
+              SqlConnection con = new SqlConnection(connectionString);
+              SqlCommand cmd = new SqlCommand(sql, con);
+              cmd.CommandType = CommandType.Text;
+              con.Open();
+   
+              try
+              {
+                  int i = cmd.ExecuteNonQuery();
+                  if (i > 0)
+                      MessageBox.Show("Registro excluído com sucesso!");
+              }
+              catch (Exception ex)
+              {
+                  MessageBox.Show("Erro: " + ex.ToString());
+              }
+              finally
+              {
+                  con.Close();
+              }
+              
+              tsbNovo.Enabled = true;
+              tsbSalvar.Enabled = false;
+              tsbCancelar.Enabled = false;
+              tsbExcluir.Enabled = false;
+              tstId.Enabled = true;
+              tsbBuscar.Enabled = true;
+              txtNome.Enabled = false;
+              txtEndereco.Enabled = false;
+              mskCEP.Enabled = false;
+              txtBairro.Enabled = false;
+              txtCidade.Enabled = false;
+              txtUF.Enabled = false;
+              mskTelefone.Enabled = false;
+              txtId.Text = "";
+              txtNome.Text = "";
+              txtEndereco.Text = "";
+              mskCEP.Text = "";
+              txtBairro.Text = "";
+              txtCidade.Text = "";
+              txtUF.Text = "";
+              mskTelefone.Text = "";
+        }
+
+        private void tsbBuscar_Click(object sender, EventArgs e)
+        {
+             
+              string sql = "SELECT * FROM CLIENTE WHERE ID=" + tstId.Text;
+   
+              SqlConnection con = new SqlConnection(connectionString);
+              SqlCommand cmd = new SqlCommand(sql, con);
+              cmd.CommandType = CommandType.Text;
+              SqlDataReader reader;
+              con.Open();
+   
+              try
+              {
+                  reader = cmd.ExecuteReader();
+                  if (reader.Read())
+                  {
+                      tsbNovo.Enabled = false;
+                      tsbSalvar.Enabled = true;
+                      tsbCancelar.Enabled = true;
+                      tsbExcluir.Enabled = true;
+                      tstId.Enabled = false;
+                      tsbBuscar.Enabled = false;
+                      txtNome.Enabled = true;
+                      txtEndereco.Enabled = true;
+                      mskCEP.Enabled = true;
+                      txtBairro.Enabled = true;
+                      txtCidade.Enabled = true;
+                      txtUF.Enabled = true;
+                      mskTelefone.Enabled = true;
+                      txtNome.Focus();
+                      txtId.Text = reader[0].ToString();
+                      txtNome.Text = reader[1].ToString();
+                      txtEndereco.Text = reader[2].ToString();
+                      mskCEP.Text = reader[3].ToString();
+                      txtBairro.Text = reader[4].ToString();
+                      txtCidade.Text = reader[5].ToString();
+                      txtUF.Text = reader[6].ToString();
+                      mskTelefone.Text = reader[7].ToString();
+                      novo = false;
+                  }
+                  else
+                      MessageBox.Show("Nenhum registro encontrado com o Id informado!");
+   
+              }
+              catch (Exception ex)
+              {
+                  MessageBox.Show("Erro: " + ex.ToString());
+              }
+              finally
+              {
+                  con.Close();
+              }
+   
+              tstId.Text = "";
+        }
+
+    }
+}
